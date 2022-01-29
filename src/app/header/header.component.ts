9a9a0c8d834b3337bf98b4173adfdf9a083d98e2
hfolcot <hfolcot@gmail.com>
2022-01-29 16:05:14 +0000

    implemented routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  @Output() toggleNavigation = new EventEmitter<string>();
-
-  toggleNavButtonClick(item) {
-    this.toggleNavigation.emit(item);
-  }
-}
+export class HeaderComponent {}
