19ac4156cb91bc66535fe5b40d3a7e216c5eb386
hfolcot <hfolcot@gmail.com>
2022-02-08 17:28:05 +0000

    updated to use subject instead of eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'recipesProject';
   visibleFeature = 'recipe';
-
-  onNavigate(item) {
-    this.visibleFeature = item;
-  }
 }
